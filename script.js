100bd0875587d997aeab8559b496d98b8d7ae1a0
Umar7007 <uumarbek83@gmail.com>
2025-05-29 16:44:52 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+
+
+let a = prompt('your password')
+
+let users = [
+    {
+        id:1,
+        name:"Umar",
+        password:1234,
+    },
+
+     {
+        id:2,
+        name:"Vali",
+        password:12345,
+    },
+     {
+        id:3,
+        name:"Xasan",
+        password:123456,
+    },
+
+     {
+        id:4,
+        name:"Xusan",
+        password:1234567,
+    },
+]
+
+console.log(users);
+
+for(i=0; i<users.lenght;  i++ ){
+    console.log(users[i]);
+    
+}
